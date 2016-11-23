179443abe2fb40378182ce6702d8ac76ba433d00
George Prince <georgeprince32@gmail.com>
2016-11-23 21:08:03 +0000

    Seats
    Seats Class with Gold, Silver & Bronze

--- ConcertBooking/src/concertbooking/Customer.java
+++ ConcertBooking/src/concertbooking/Customer.java
@@ -1,3 +1,5 @@
+package concertbooking;
+
 public class Customer {
 
 	//Variables
---
+++ ConcertBooking/src/concertbooking/Seats.java
@@ -0,0 +1,222 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concertbooking;
+
+/**
+ *
+ * @author graem
+ */
+public class Seats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [][] seatNumbers;
+        
+        seatNumbers = new String [9][10];
+        
+        seatNumbers[0][0] = "A1";
+        seatNumbers[0][1] = "A2";
+        seatNumbers[0][2] = "A3";
+        seatNumbers[0][3] = "A4";
+        seatNumbers[0][4] = "A5";
+        seatNumbers[0][5] = "A6";
+        seatNumbers[0][6] = "A7";
+        seatNumbers[0][7] = "A8";
+        seatNumbers[0][8] = "A9";
+        seatNumbers[0][9] = "A10";
+        
+        seatNumbers[1][0] = "B1";
+        seatNumbers[1][1] = "B2";
+        seatNumbers[1][2] = "B3";
+        seatNumbers[1][3] = "B4";
+        seatNumbers[1][4] = "B5";
+        seatNumbers[1][5] = "B6";
+        seatNumbers[1][6] = "B7";
+        seatNumbers[1][7] = "B8";
+        seatNumbers[1][8] = "B9";
+        seatNumbers[1][9] = "B10";
+        
+        seatNumbers[2][0] = "C1";
+        seatNumbers[2][1] = "C2";
+        seatNumbers[2][2] = "C3";
+        seatNumbers[2][3] = "C4";
+        seatNumbers[2][4] = "C5";
+        seatNumbers[2][5] = "C6";
+        seatNumbers[2][6] = "C7";
+        seatNumbers[2][7] = "C8";
+        seatNumbers[2][8] = "C9";
+        seatNumbers[2][9] = "C10";
+        
+        seatNumbers[3][0] = "D1";
+        seatNumbers[3][1] = "D2";
+        seatNumbers[3][2] = "D3";
+        seatNumbers[3][3] = "D4";
+        seatNumbers[3][4] = "D5";
+        seatNumbers[3][5] = "D6";
+        seatNumbers[3][6] = "D7";
+        seatNumbers[3][7] = "D8";
+        seatNumbers[3][8] = "D9";
+        seatNumbers[3][9] = "D10";
+        
+        seatNumbers[4][0] = "E1";
+        seatNumbers[4][1] = "E2";
+        seatNumbers[4][2] = "E3";
+        seatNumbers[4][3] = "E4";
+        seatNumbers[4][4] = "E5";
+        seatNumbers[4][5] = "E6";
+        seatNumbers[4][6] = "E7";
+        seatNumbers[4][7] = "E8";
+        seatNumbers[4][8] = "E9";
+        seatNumbers[4][9] = "E10";
+        
+        seatNumbers[5][0] = "F1";
+        seatNumbers[5][1] = "F2";
+        seatNumbers[5][2] = "F3";
+        seatNumbers[5][3] = "F4";
+        seatNumbers[5][4] = "F5";
+        seatNumbers[5][5] = "F6";
+        seatNumbers[5][6] = "F7";
+        seatNumbers[5][7] = "F8";
+        seatNumbers[5][8] = "F9";
+        seatNumbers[5][9] = "F10";
+        
+        seatNumbers[6][0] = "G1";
+        seatNumbers[6][1] = "G2";
+        seatNumbers[6][2] = "G3";
+        seatNumbers[6][3] = "G4";
+        seatNumbers[6][4] = "G5";
+        seatNumbers[6][5] = "G6";
+        seatNumbers[6][6] = "G7";
+        seatNumbers[6][7] = "G8";
+        seatNumbers[6][8] = "G9";
+        seatNumbers[6][9] = "G10";
+        
+        seatNumbers[7][0] = "H1";
+        seatNumbers[7][1] = "H2";
+        seatNumbers[7][2] = "H3";
+        seatNumbers[7][3] = "H4";
+        seatNumbers[7][4] = "H5";
+        seatNumbers[7][5] = "H6";
+        seatNumbers[7][6] = "H7";
+        seatNumbers[7][7] = "H8";
+        seatNumbers[7][8] = "H9";
+        seatNumbers[7][9] = "H10";
+        
+        seatNumbers[8][0] = "I1";
+        seatNumbers[8][1] = "I2";
+        seatNumbers[8][2] = "I3";
+        seatNumbers[8][3] = "I4";
+        seatNumbers[8][4] = "I5";
+        seatNumbers[8][5] = "I6";
+        seatNumbers[8][6] = "I7";
+        seatNumbers[8][7] = "I8";
+        seatNumbers[8][8] = "I9";
+        seatNumbers[8][9] = "I10";
+        
+        System.out.println(seatNumbers[0][9]); 
+        System.out.println(seatNumbers[1][9]);
+        System.out.println(seatNumbers[2][9]);
+        System.out.println(seatNumbers[3][9]);
+        System.out.println(seatNumbers[4][9]);
+        System.out.println(seatNumbers[5][9]);
+        System.out.println(seatNumbers[6][9]);
+        System.out.println(seatNumbers[7][9]);
+        System.out.println(seatNumbers[8][9]);
+        
+        System.out.println(seatNumbers[0][8]);
+        System.out.println(seatNumbers[1][8]);
+        System.out.println(seatNumbers[2][8]);
+        System.out.println(seatNumbers[3][8]);
+        System.out.println(seatNumbers[4][8]);
+        System.out.println(seatNumbers[5][8]);
+        System.out.println(seatNumbers[6][8]);
+        System.out.println(seatNumbers[7][8]);
+        System.out.println(seatNumbers[8][8]);
+        
+        System.out.println(seatNumbers[0][7]);
+        System.out.println(seatNumbers[1][7]);
+        System.out.println(seatNumbers[2][7]);
+        System.out.println(seatNumbers[3][7]);
+        System.out.println(seatNumbers[4][7]);
+        System.out.println(seatNumbers[5][7]);
+        System.out.println(seatNumbers[6][7]);
+        System.out.println(seatNumbers[7][7]);
+        System.out.println(seatNumbers[8][7]);
+        
+        System.out.println(seatNumbers[0][6]);
+        System.out.println(seatNumbers[1][6]);
+        System.out.println(seatNumbers[2][6]);
+        System.out.println(seatNumbers[3][6]);
+        System.out.println(seatNumbers[4][6]);
+        System.out.println(seatNumbers[5][6]);
+        System.out.println(seatNumbers[6][6]);
+        System.out.println(seatNumbers[7][6]);
+        System.out.println(seatNumbers[8][6]);
+        
+        System.out.println(seatNumbers[0][5]);
+        System.out.println(seatNumbers[1][5]);
+        System.out.println(seatNumbers[2][5]);
+        System.out.println(seatNumbers[3][5]);
+        System.out.println(seatNumbers[4][5]);
+        System.out.println(seatNumbers[5][5]);
+        System.out.println(seatNumbers[6][5]);
+        System.out.println(seatNumbers[7][5]);
+        System.out.println(seatNumbers[8][5]);
+        
+        System.out.println(seatNumbers[0][4]);
+        System.out.println(seatNumbers[1][4]);
+        System.out.println(seatNumbers[2][4]);
+        System.out.println(seatNumbers[3][4]);
+        System.out.println(seatNumbers[4][4]);
+        System.out.println(seatNumbers[5][4]);
+        System.out.println(seatNumbers[6][4]);
+        System.out.println(seatNumbers[7][4]);
+        System.out.println(seatNumbers[8][4]);
+        
+        System.out.println(seatNumbers[0][3]);
+        System.out.println(seatNumbers[1][3]);
+        System.out.println(seatNumbers[2][3]);
+        System.out.println(seatNumbers[3][3]);
+        System.out.println(seatNumbers[4][3]);
+        System.out.println(seatNumbers[5][3]);
+        System.out.println(seatNumbers[6][3]);
+        System.out.println(seatNumbers[7][3]);
+        System.out.println(seatNumbers[8][3]);
+        
+        System.out.println(seatNumbers[0][2]);
+        System.out.println(seatNumbers[1][2]);
+        System.out.println(seatNumbers[2][2]);
+        System.out.println(seatNumbers[3][2]);
+        System.out.println(seatNumbers[4][2]);
+        System.out.println(seatNumbers[5][2]);
+        System.out.println(seatNumbers[6][2]);
+        System.out.println(seatNumbers[7][2]);
+        System.out.println(seatNumbers[8][2]);
+        
+        System.out.println(seatNumbers[0][1]);
+        System.out.println(seatNumbers[1][1]);
+        System.out.println(seatNumbers[2][1]);
+        System.out.println(seatNumbers[3][1]);
+        System.out.println(seatNumbers[4][1]);
+        System.out.println(seatNumbers[5][1]);
+        System.out.println(seatNumbers[6][1]);
+        System.out.println(seatNumbers[7][1]);
+        System.out.println(seatNumbers[8][1]);
+        
+        System.out.println(seatNumbers[0][0]);
+        System.out.println(seatNumbers[1][0]);
+        System.out.println(seatNumbers[2][0]);
+        System.out.println(seatNumbers[3][0]);
+        System.out.println(seatNumbers[4][0]);
+        System.out.println(seatNumbers[5][0]);
+        System.out.println(seatNumbers[6][0]);
+        System.out.println(seatNumbers[7][0]);
+        System.out.println(seatNumbers[8][0]);
+    }      
+}
+
