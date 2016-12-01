b6a99d4038095e38e99ff4fe5c1d626e6a2f11dc
George Prince <georgeprince32@gmail.com>
2016-12-01 20:04:04 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ConcertBooking/src/concertbooking/ConcertHall.java

--- ConcertBooking/src/concertbooking/ConcertHall.java
+++ ConcertBooking/src/concertbooking/ConcertHall.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package concertbooking;
 
     public class ConcertHall {
         
-        Seat[][] seats;
+        private Seat[][] seats;
+        private int soldCount;
+        
         
         public ConcertHall()
         {
             seats = new Seat[9][10];
+            soldCount = 0;
         }
         
         public Seat getSeat(int x, int y)
         {
             return seats[x][y];
+        }
+        
+        public int getNumberOfSeatsAvailable()
+        {
+            for (int i=0; i<9; i++) {
+                for (int j=0; j<10; j++) {
+                Seat seat = getSeat(i, j);
+                    if (seat.getBooked())
+                    {
+                        soldCount++;
+                    }
+                }
+            }
+            return 0;
+        }      
+        public int getNumberOfSeatsSold()
+        {
+            for (int i=0; i<9; i--) {
+                for (int j=0; j<10; j--) {
+                Seat seat = getSeat(i, j);
+                    if (seat.getBooked())
+                    {
+                        soldCount--;
+                    }
+                }
+            }
+            return 0;
         }
 }
