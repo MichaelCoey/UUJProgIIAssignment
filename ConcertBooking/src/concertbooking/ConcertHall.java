b077acca378053be3e14b07294573f732edaa804
dmckee763 <dmckee763@gmail.com>
2016-12-02 11:06:10 +0000

    Update ConcertHall
    Updated ConcertHall

--- ConcertBooking/src/concertbooking/ConcertHall.java
+++ ConcertBooking/src/concertbooking/ConcertHall.java
@@ -5,7 +5,6 @@ package concertbooking;
         //Concert Hall Atributes
         private Seat[][] seats;
         private int soldCount;
-        private int availableCount;
         private int totalSeats = 90;
         
         
@@ -13,7 +12,38 @@ package concertbooking;
         //Set the Array Size to 9 rows and 10 columns and Seats sold to 0
         {
             seats = new Seat[9][10];
-            soldCount = 0;
+            assignSeatingTiers();
+            soldCount = getNumberOfSeatsSold();
+        }
+        
+        private void assignSeatingTiers()
+        {
+            //Assign Gold seats
+            for(int i=0; i<3; i++)
+            {
+                for(int j=0; j<10; j++)
+                {
+                    seats[i][j] = new Gold();
+                }
+            }
+
+            //Assign Silver seats
+            for(int i=3; i<6; i++)
+            {
+                for(int j=0; j<10; j++)
+                {
+                    seats[i][j] = new Silver();
+                }
+            }
+
+            //Assign Bronze seats
+            for(int i=6; i<9; i++)
+            {
+                for(int j=0; j<10; j++)
+                {
+                    seats[i][j] = new Bronze();
+                }
+            }
         }
         
         public Seat getSeat(int x, int y)
@@ -22,11 +52,11 @@ package concertbooking;
             return seats[x][y];
         }
         
-        public int getNumberOfSeatsSold()
+        private int getNumberOfSeatsSold()
         {
             for (int i = 0; i < 9; soldCount++) {
                 for (int j = 0; j < 10; soldCount++) {
-                Seat seat = getSeat(i, j);
+                    Seat seat = getSeat(i, j);
                     if (seat.getBooked())
                     {
                         soldCount++;
@@ -35,6 +65,7 @@ package concertbooking;
             }
             return soldCount;
         }        
+        
         
         //public int getNumberOfSeatsAvailable()
         //{
