636470f258b0f67113d01b0142ab38b08e34b62f
George Prince <georgeprince32@gmail.com>
2016-12-02 10:53:45 +0000

    Update 02/12/16

--- ConcertBooking/src/concertbooking/ConcertHall.java
+++ ConcertBooking/src/concertbooking/ConcertHall.java
@@ -2,25 +2,30 @@ package concertbooking;
 
     public class ConcertHall {
         
+        //Concert Hall Atributes
         private Seat[][] seats;
         private int soldCount;
+        private int availableCount;
+        private int totalSeats = 90;
         
         
         public ConcertHall()
+        //Set the Array Size to 9 rows and 10 columns and Seats sold to 0
         {
             seats = new Seat[9][10];
             soldCount = 0;
         }
         
         public Seat getSeat(int x, int y)
+        //To get the exact seat find its row and column value
         {
             return seats[x][y];
         }
         
-        public int getNumberOfSeatsAvailable()
+        public int getNumberOfSeatsSold()
         {
-            for (int i=0; i<9; i++) {
-                for (int j=0; j<10; j++) {
+            for (int i = 0; i < 9; soldCount++) {
+                for (int j = 0; j < 10; soldCount++) {
                 Seat seat = getSeat(i, j);
                     if (seat.getBooked())
                     {
@@ -28,19 +33,13 @@ package concertbooking;
                     }
                 }
             }
-            return 0;
-        }      
-        public int getNumberOfSeatsSold()
-        {
-            for (int i=0; i<9; i--) {
-                for (int j=0; j<10; j--) {
-                Seat seat = getSeat(i, j);
-                    if (seat.getBooked())
-                    {
-                        soldCount--;
-                    }
-                }
-            }
-            return 0;
-        }
+            return soldCount;
+        }        
+        
+        //public int getNumberOfSeatsAvailable()
+        //{
+            //totalSeats - soldCount = availableCount;
+            
+            //return availableCount;
+        //}      
 }
