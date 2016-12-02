d7b44c1c63ea06f3436524daf41971f7020f7a3a
George Prince <georgeprince32@gmail.com>
2016-12-02 17:00:57 +0000

    Update 2 02/12/16

--- ConcertBooking/src/concertbooking/ConcertHall.java
+++ ConcertBooking/src/concertbooking/ConcertHall.java
@@ -5,15 +5,14 @@ package concertbooking;
         //Concert Hall Atributes
         private Seat[][] seats;
         private int soldCount;
-        private int totalSeats = 90;
+        private static final int TOTAL_SEATS = 90;
         
-        
+        //Set the Array Size to 9 rows and 10 columns and Seats sold to 0       
         public ConcertHall()
-        //Set the Array Size to 9 rows and 10 columns and Seats sold to 0
         {
             seats = new Seat[9][10];
             assignSeatingTiers();
-            soldCount = getNumberOfSeatsSold();
+            soldCount = calculateNumberOfSeatsSold();
         }
         
         private void assignSeatingTiers()
@@ -46,16 +45,17 @@ package concertbooking;
             }
         }
         
-        public Seat getSeat(int x, int y)
         //To get the exact seat find its row and column value
+        public Seat getSeat(int x, int y)
         {
             return seats[x][y];
         }
         
-        private int getNumberOfSeatsSold()
+        //Find out how many Seats are Sold
+        private int calculateNumberOfSeatsSold()
         {
-            for (int i = 0; i < 9; soldCount++) {
-                for (int j = 0; j < 10; soldCount++) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 10; j++) {
                     Seat seat = getSeat(i, j);
                     if (seat.getBooked())
                     {
@@ -64,13 +64,31 @@ package concertbooking;
                 }
             }
             return soldCount;
-        }        
+        }
         
+        //Return the number of Seats Sold
+        private int getNumberOfSeatsSold()
+        {
+            return soldCount;
+        }
         
-        //public int getNumberOfSeatsAvailable()
-        //{
-            //totalSeats - soldCount = availableCount;
+        //Find out the updated amount of Seats Sold
+        private void addSeats(int numSeats)
+        {
+            soldCount += numSeats;
+        }   
+         
+        //Find out how many Seats are Available
+        public int getNumberOfSeatsAvailable()
+        {
+            int availableCount = TOTAL_SEATS - soldCount;
             
-            //return availableCount;
-        //}      
+            return availableCount;
+        } 
+        
+        //Find out the updated amount of Seats Available
+        private void removeSeats(int numSeats)
+        {
+            soldCount -= numSeats;
+        } 
 }
